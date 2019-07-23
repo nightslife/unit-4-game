ae0f0ff4cb35f37f6b547d16f4de281367dcf21c
nightslife <ragefail1212@gmail.com>
2019-07-23 13:58:55 -0400

    game script built. had bug of multiple clicks registering on every new game. fixed with unbind

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var goal = 0;
+var goal = Math.floor((Math.random()*101)+19)
 var currentScore = 0;
 var wins = 0;
 var losses = 0;
@@ -9,47 +9,55 @@ var sapphire = 0;
 
 
 function newGame() {
-    goal = Math.floor((Math.random()*101)+19)
     currentScore = 0;
-    ruby = Math.floor(Math.random()*12)
-    emerald = Math.floor(Math.random()*12)
-    onyx = Math.floor(Math.random()*12)
-    sapphire = Math.floor(Math.random()*12)
+    ruby = Math.floor((Math.random()*11)+1)
+    emerald = Math.floor((Math.random()*11)+1)
+    onyx = Math.floor((Math.random()*11)+1)
+    sapphire = Math.floor((Math.random()*11)+1)
 
 
     $("#goal").text(goal)
 
-    $("#ruby").on("click",function() {
+    $("#ruby").unbind().on("click",function() {
         currentScore = currentScore + ruby
         console.log("Ruby added: "+ruby)
     })
 
-    $("#emerald").on("click",function() {
+    $("#emerald").unbind().click(function() {
         currentScore = currentScore + emerald
         console.log("Emerald added: "+emerald)
     })
 
-    $("#sapphire").on("click",function() {
+    $("#sapphire").unbind().on("click",function() {
         currentScore = currentScore + sapphire
         console.log("Sapphire added: "+sapphire)
     })
 
-    $("#onyx").on("click",function() {
+    $("#onyx").unbind().on("click",function() {
         currentScore = currentScore + onyx
         console.log("Onyx added: "+onyx)
     })
 
 
     $(".gem").on("click",function(){
+        $("#scoreMessage").html("Current Score:  <span id='score'>0</span>");
         $("#score").text(currentScore)
         if(currentScore === goal){
             wins++;
             // You win message here
+            goal = Math.floor((Math.random()*101)+19)
+            $("#goal").text(goal);
+            $("#scoreMessage").text("You won! There is a new goal if you wish to press your luck.");
+            $("#score").text("");
             newGame()
     
         } else if(currentScore >= goal){
             losses++
             //You lose message here
+            goal = Math.floor((Math.random()*101)+19)
+            $("#goal").text(goal);
+            $("#scoreMessage").text("You lost! There is a new goal if you wish to turn your luck around.");
+            $("#score").text("");
             newGame()
         }
     })
